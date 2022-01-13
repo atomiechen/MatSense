1a63929157b6cb581ed32076b7ecd17d5dc40d38
Atomie CHEN <atomic_cwh@163.com>
2022-01-13 15:45:42 +0800

    Major: setup.py adds visualization dependencies
    'pyqtgraph' as extras_require

--- setup.py
+++ setup.py
@@ -158,6 +158,7 @@ setuptools.setup(
         "pyserial>=3.5",
         "PyYAML>=5.4.1",
         "pyparsing>=2.4.7",
+        "matplotlib>=3.3",
     ],
 
     # List additional groups of dependencies here (e.g. development
@@ -168,10 +169,15 @@ setuptools.setup(
     #
     # Similar to `install_requires` above, these must be valid existing
     # projects.
-    # extras_require={  # Optional
-    #     'dev': ['check-manifest'],
-    #     'test': ['coverage'],
-    # },
+    extras_require={  # Optional
+        'pyqtgraph': [
+            "pyqtgraph==0.11.*",
+            "PyOpenGL~=3.1.5",
+            "PyQt5==5.12.* ; python_version == '3.7'",
+            "PyQt5==5.15.* ; python_version == '3.8'",
+            "PyQt6==6.0.* ; python_version == '3.9'",
+        ],
+    },
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.
